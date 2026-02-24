7f82c8b0135cc1cb3121f15fd74a22cb57abd45a
theoppon <theoppon@student.42belgium.be>
2026-02-24 22:20:29 +0100

    linked list

---
+++ includes/push_swap.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: theoppon <theoppon@student.42belgium.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 18:37:04 by theoppon          #+#    #+#             */
+/*   Updated: 2026/02/24 22:19:45 by theoppon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+
+typedef struct	s_node
+{
+	long		number;
+	struct s_node 	*next;
+	struct s_node	*prev;
+}t_node;
+
+typedef struct	s_stack
+{
+	t_node	*a;
+	t_node	*b;
+}t_stack;
+
+//Linked list funtcions: lst_utils.c
+
+t_node	*ft_lstnew(long number);
+t_node	*ft_lstlast(t_node *head);
+void	ft_lstadd_front(t_node **head, t_node *new);
+void	ft_lstadd_back(t_node **head, t_node *new);
+void	ft_lstclear(t_node **head);		
+
+
+#endif
---
+++ srcs/lst_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lst_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: theoppon <theoppon@student.42belgium.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 19:14:47 by theoppon          #+#    #+#             */
+/*   Updated: 2026/02/24 22:19:42 by theoppon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_node	*ft_lstnew(long number)
+{
+	t_node	*new_node;
+
+	new_node = malloc(sizeof(t_node));
+	new_node->number = number;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	return (new_node);
+}
+
+void	ft_lstadd_front(t_node **head, t_node *new)
+{
+	new->prev = NULL;
+	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+}
+
+t_node	*ft_lstlast(t_node *head)
+{
+	t_node	*temp;
+
+	if (!head)
+		return (NULL);
+	temp = head;
+	while (temp->next)
+		temp = temp->next;
+	return (temp);
+}
+
+void	ft_lstadd_back(t_node **head, t_node *new)
+{
+	t_node	*last;
+
+	if (!head)
+	{
+		*head = new;
+		new->prev = NULL;
+		new->next = NULL;
+		return ;
+	}
+	last = ft_lstlast(*head);
+	new->next = NULL;
+	new->prev = last;
+	last->next = new;
+}
+
+void	ft_lstclear(t_node **head)
+{
+
+}
